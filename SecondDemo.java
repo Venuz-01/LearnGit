c7fb129d6b34addae501eed95e17f051fd62e676
Venuz-01 <adicherlavenugopal@gmail.com>
2025-08-21 12:42:36 +0530

    SecondDemo committed

---
+++ SecondDemo.java
@@ -0,0 +1,15 @@
+class SecondDemo {
+    public static void main(String args[])
+    {
+        System.out.println('hey There!');
+        int n=5;
+        if(n%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
